d0df2b9e350c0705f01bcb8a6b26ae37eebed2b9
Donavon Swartout <wm7123we@go.minnstate.edu>
2019-12-01 22:30:55 -0600

    Partially completed Ex3A

---
+++ dswartout1730ex3a/ex3aLoops.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dswartout1730ex3a
+{
+    public class Ex3aLoops
+    {
+        //public static string Calc0()
+        //{
+        //    int i = 1, sum = 0;
+        //    while (i < 5)
+        //    {
+        //        sum += i;
+        //        i++;
+        //    }
+
+        //    return sum.ToString();
+        //}
+        public static string Calc0(string strMonths, string strMonthlyPayment, string strMonthlyInterestRate)
+        {
+            double futureValue = 0.0;
+            try
+            {
+                int months = Int32.Parse(strMonths);
+                double monthlyPayment = Double.Parse(strMonthlyPayment);
+                double monthlyInterestRate = Double.Parse(strMonthlyInterestRate);
+                int i = 1;
+                while (i <= months)
+                {
+                    futureValue = (futureValue + monthlyPayment) *
+                        (1 + monthlyInterestRate);
+                    i++;
+                }
+            }
+            catch
+            {
+                return "Invalid input";
+            }
+
+            return futureValue.ToString("n2");
+        }
+        public static string Calc1(string strMonths, string strMonthlyPayment, string strMonthlyInterestRate)
+        {
+            double futureValue = 0.0;
+            try
+            {
+                int months = Int32.Parse(strMonths);
+                double monthlyPayment = Double.Parse(strMonthlyPayment);
+                double monthlyInterestRate = Double.Parse(strMonthlyInterestRate);
+                int i = 1;
+                do
+                {
+                    futureValue = (futureValue + monthlyPayment) *
+                        (1 + monthlyInterestRate);
+                    i++;
+                }
+                while (i <= months);
+            }
+            catch
+            {
+                return "Invalid input";
+            }
+
+            return futureValue.ToString("n2");
+        }
+        //Example of If that stores 0 through 3 in a string
+        //public static string Calc2()
+        //{
+        //    string numbers = "";
+        //    for (int i = 0; i < 4; i++)
+        //        numbers += i + " ";
+
+        //    return numbers;
+        //}
+        //public static string Calc2()
+        //{
+        //    string numbers = "";
+        //    for (int i = 8; i < 2; i -= 2)
+        //    {
+        //        numbers += i.ToString();
+        //        numbers += " ";
+        //    }
+
+        //    return numbers;
+        //}
+        public static string Calc2(string strMonths, string strMonthlyPayment, string strMonthlyInterestRate)
+        {
+            double futureValue = 0.0;
+            try
+            {
+                int months = Int32.Parse(strMonths);
+                double monthlyPayment = Double.Parse(strMonthlyPayment);
+                double monthlyInterestRate = Double.Parse(strMonthlyInterestRate);
+                for (int i = 1; i <= months; i++)
+                {
+                    futureValue = (futureValue + monthlyPayment) * 
+                        (1 + monthlyInterestRate);
+                }
+            }
+            catch
+            {
+                return "Invalid input";
+            }
+
+            return futureValue.ToString("n2");
+        }
+        public static string Calc3(string strStart, string strEnd, string strIncrement)
+        {
+            string result = "";
+            try
+            {
+                int start = Int32.Parse(strStart);
+                int end = Int32.Parse(strEnd);
+                int increment = Int32.Parse(strIncrement);
+                if (increment <= 0) throw new Exception();
+
+                int i = start;
+                while (i < end)
+                {
+
+                    result += i.ToString() + " ";
+                    i += increment;
+                }
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+
+            return result;
+        }
+
+        public static string Calc4(string strStart, string strEnd, string strIncrement)
+        {
+            string result = "";
+            try
+            {
+                int start = Int32.Parse(strStart);
+                int end = Int32.Parse(strEnd);
+                int increment = Int32.Parse(strIncrement);
+                if (increment <= 0) throw new Exception();
+
+                int i = start;
+                do
+                {
+                    result += i.ToString() + " ";
+                    i += increment;
+                }
+                while (i < end);
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+
+            return result;
+        }
+
+        public static string Calc5(string strStart, string strEnd, string strIncrement)
+        {
+            string result = "";
+            try
+            {
+                int start = Int32.Parse(strStart);
+                int end = Int32.Parse(strEnd);
+                int increment = Int32.Parse(strIncrement);
+                if (increment <= 0) throw new Exception();
+
+                for (int i = start; i < end; i += increment)
+                    result += i + " ";
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+            
+            return result;
+        }
+
+        public static string Calc6(string strNumbers, string strCount)
+        {
+            string result = "";
+            int startindex = 0;
+
+            try
+            {
+                
+            }
+            catch
+            {
+                result = "Invalid input";
+            }
+
+            return result;
+        }
+
+        public static string Calc7(string strNumbers, string strCount)
+        {
+            string result = "";
+
+            return result;
+        }
+
+        public static string Calc8(string strNumbers, string strCount)
+        {
+            string result = "";
+
+            return result;
+        }
+    }
+}
---
+++ dswartoutex3aUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dswartoutex3aUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dswartoutex3aUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a08c1fb9-ac69-4a8c-b153-591b32c63b6d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
